e702a0a3a76e6e39c4c9f88390408a99d5c2cc89
Gijs Jorissen <gijs@360ecommerce.nl>
2014-06-09 15:08:23 +0200

    Set up meta boxes

--- classes/class-content-types.php
+++ classes/class-content-types.php
@@ -9,6 +9,12 @@ class Lookbooq_Content_Types
 		// Init
 		add_action( 'init', array( &$this, 'register_post_types' ) );
 		add_action( 'init', array( &$this, 'register_taxonomies' ) );
+
+		// Meta boxes
+		add_action( 'add_meta_boxes', array( &$this, 'add_pointers_meta_box' ) );
+
+		// Save
+		add_action( 'save_post', array( &$this, 'save_post' ) );
 	}
 
 	/**
@@ -86,5 +92,62 @@ class Lookbooq_Content_Types
 		) );
 
 		register_taxonomy( 'lookbooq', array( 'piqture' ), $args );
+	}
+
+	function add_pointers_meta_box()
+	{
+		add_meta_box(
+			'lookbooq_pointers',
+			__( 'Pointers', 'lookbooq' ),
+			array( &$this, '_add_pointers_meta_box' ),
+			'piqture'
+		);
+	}
+
+	function _add_pointers_meta_box()
+	{
+		wp_nonce_field( 'lookbooq_meta', 'lookbooq_nonce' ); ?>
+			<input type="hidden" name="lookbooq[__activate]" />
+			<div class="lookbooq">
+				<div class="lookbooq-bundles">
+					<ul>
+						<li>
+							<input type="text" name="lookbooq[0][left]" />
+						</li>
+						<li>
+							<input type="text" name="lookbooq[0][top]" />
+						</li>
+					</ul>
+				</div>
+			</div>
+		<?php
+	}
+
+	function save_post()
+	{
+		// Deny the wordpress autosave function
+		if( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+			return;
+		}
+
+		// Verify nonce
+		if( ! ( isset( $_POST['lookbooq_nonce'] ) && wp_verify_nonce( $_POST['lookbooq_nonce'], 'lookbooq_meta' ) ) ) {
+			return;
+		}
+
+		// Is the current user capable to edit this post
+		foreach( array( 'piqture' ) as $post_type ) {
+			if( ! current_user_can( get_post_type_object( $post_type )->cap->edit_post, $post_id ) ) {
+				return;
+			}
+		}
+
+		// Values
+		$values = isset( $_POST['lookbooq'] ) ? $_POST['lookbooq'] : array();
+
+		// Save
+		if( ! empty( $values ) ) {
+			// Save
+		}
 	}
 }
